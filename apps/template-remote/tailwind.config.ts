ae660d25a9f7f9b94717bdfdc6a4eba26f946229
Akif UNAL <akif.unal@tomgidital.com.tr>
2023-09-14 01:45:55 +0300

    chore: configs updated

--- apps/template-remote/tailwind.config.ts
+++ apps/template-remote/tailwind.config.ts
@@ -1,10 +1,3 @@
 import baseConfig from 'ui/remote-tw.config'
 
 export default baseConfig
-
-// import type { Config } from 'tailwindcss'
-
-// export default {
-// 	content: [...baseConfig.content],
-// 	presets: [baseConfig],
-// } satisfies Config
